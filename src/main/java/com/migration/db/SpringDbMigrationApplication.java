7a6a4ed9a33c126a1bd7534aa0f4fd668cfbac62
Sushant Sinha <sushant.sinha@medvanture.com>
2023-05-04 11:39:05 +0530

    initial commit

---
+++ src/main/java/com/migration/db/SpringDbMigrationApplication.java
@@ -0,0 +1,13 @@
+package com.migration.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDbMigrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDbMigrationApplication.class, args);
+    }
+
+}
